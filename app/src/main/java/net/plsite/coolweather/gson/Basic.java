a5dedacb3cf367f2c95adbc76b3d61ef0a9c73cf
Bingo <yanhuibin315@sina.com>
2019-03-11 15:31:19 +0800

    新增布局文件

---
+++ app/src/main/java/net/plsite/coolweather/gson/Basic.java
@@ -0,0 +1,16 @@
+package net.plsite.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
